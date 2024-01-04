3d8d7a760ac40058e17fb8ee15bad510c1d048b2
MaheshDoiphode <maheshdoiphode0007@gmail.com>
2024-01-04 20:07:23 +0530

    endpoint added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +12,13 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
+}
+
+@RestController
+class Hello{
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
 }
